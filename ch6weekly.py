3eda7def69941370823a2840785c058303418ca8
Pandaradox <jacardarella@gmail.com>
2018-08-21 16:16:46 -0500

    Added placeholder for code with def main()

--- ch6weekly.py
+++ ch6weekly.py
@@ -1,2 +1,8 @@
 #This is just a placeholder for the coming of chapter 6 weekly assign
-print("Ham and Eggs")
+#####Python is about to Ssstrike
+def main():
+    print("Ham and Eggs")
+
+
+if __name__ == "__main__":
+    main()
