06c6db150b2b61f8f538b687697ba205adec6a00
Pandaradox <jacardarella@gmail.com>
2018-09-20 15:04:15 -0500

    13 weekly completed

--- ch13weekly.py
+++ ch13weekly.py
@@ -1,10 +1,39 @@
 
-# ########################################
+# ########################################Chatbot
+class Chatbot():
+    """ An object that can engage in rudimentary conversation with a human. """
+
+    def __init__(self, name):
+        self.name = name
+
+    def greeting(self):
+        """ Returns the Chatbot's way of introducing itself. """
+        return "Hello, my name is " + self.name
+
+    def response(self, prompt_from_human):
+        """ Returns the Chatbot's response to something the human said. """
+        return("It is very interesting that you say: '" + prompt_from_human + "'")
+
+
+class BoredChatbot(Chatbot):
+    def __init__(self, name):
+        super(BoredChatbot, self).__init__(name)
+
+    def response(self, prompt_from_human):
+        if len(str(prompt_from_human)) > 20:
+            return("zzz... Oh excuse me, I dozed off reading your essay.")
+        else:
+            return(Chatbot.response(self, prompt_from_human))
 
 
 def main():
 
-    print("Ham and Eggs")
+    # make a chatbot
+    sally = BoredChatbot("Sally")
+    # introduce the chatbot and allow the user to say something
+    human_message = input(sally.greeting())
+    # respond to the user
+    print(sally.response(human_message))
 
 
 if __name__ == "__main__":
